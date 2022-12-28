a3b21de80e4c93f083dc1152c7d0b9784fe4b7b5
Milosz Lewandowski <milosz.lewandowski@icloud.com>
2022-12-28 21:38:02 +0100

    scroll animation project

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const boxes = document.querySelectorAll('.box');
 window.addEventListener('scroll', checkBoxes);
 
 function checkBoxes() {
-  const triggerBottom = window.innerHeight * 0.2;
+  const triggerBottom = window.innerHeight * 0.5;
 
   boxes.forEach((box) => {
     const boxTop = box.getBoundingClientRect().top;
